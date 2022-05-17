1a1230e2512f786831881a311a0d6c0429195910
CG05 <sungcjfrb2@gmail.com>
2022-05-17 22:40:10 +0900

    form은 쓰레기다.

--- Node.js/getpost.js
+++ Node.js/getpost.js
@@ -1,8 +1,11 @@
 /* getpost.js */
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true })); // encode once : true
+app.use(bodyParser.json());
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
