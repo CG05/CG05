877291eeac4276a87b741b52c66331bd1d40a0f2
ChulkyuSung <chulkyu@MacBookPro.local>
2022-05-27 05:30:52 +0900

    SocketChat Complete

--- Node.js/socketChat/public/js/login.js
+++ Node.js/socketChat/public/js/login.js
@@ -1,112 +1,94 @@
-'use strict';
+import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js';
+const socket = io();
+//connect to the host
+const loginForm = document.querySelector('.login-form');
+const loginIdInput = document.querySelector('.login-form input.id');
+const loginPwInput = document.querySelector('.login-form input.pw');
+const loginSubmit = document.querySelector('.login-form button#login');
+const signUpButton = document.querySelector('.login-form button#signUp');
+const signUpForm = document.querySelector('.signUp-form');
+const signUpIdInput = document.querySelector('.signUp-form input.id');
+const signUpPwInput = document.querySelector('.signUp-form input.pw');
+const signUpCheckInput = document.querySelector('.signUp-form input.check');
+const signUpSubmit = document.querySelector('.signUp-form button#signUp');
+const chat = document.querySelector('div.chat');
 
-const loginForm = document.querySelector(".login-form");
-const loginIdInput = document.querySelector(".login-form input.id");
-const loginPwInput = document.querySelector(".login-form input.pw");
-const loginSubmit = document.querySelector(".login-form button#login");
-const signUpButton = document.querySelector(".login-form button#signUp");
-const signUpForm = document.querySelector(".signUp-form");
-const signUpIdInput = document.querySelector(".signUp-form input.id");
-const signUpPwInput = document.querySelector(".signUp-form input.pw");
-const signUpCheckInput = document.querySelector(".signUp-form input.check");
-const signUpSubmit = document.querySelector(".signUp-form button#signUp");
-const chat = document.querySelector("div.chat");
+const HIDDEN_CLASSNAME = 'hidden';
+const USERID_KEY = 'userId';
+const EMPTY_VALUE = '';
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERID_KEY = "userId";
-const USERDB_KEY = "userDb";
-const EMPTY_VALUE = "";
-
-let userDb = [
-  { id: "admin", pw: "1234" },
-  { id: "user0", pw: "5678" },
-];
-writeDb();
-
-function writeDb() {
-  localStorage.setItem(USERDB_KEY, JSON.stringify(userDb));
-
+function loginEmit(id, pw) {
+	const user = {
+		id: id,
+		pw: pw,
+	};
+	socket.emit('login', user);
 }
+socket.on('login', (req) => {
+	console.log(req);
+	if (!req.auth) return alert('Cannot authorize. Please check again.');
 
-function readDb() {
-  const _userDb = JSON.parse(localStorage.getItem(USERDB_KEY));
-  return _userDb;
+	loginForm.classList.add(HIDDEN_CLASSNAME);
+	successLogin(req.id);
+});
+
+function signUpEmit(id, pw) {
+	const user = {
+		id: id,
+		pw: pw,
+	};
+
+	socket.emit('signUp', user);
 }
+socket.on('signUp', (unsigned) => {
+	if (!unsigned) return alert('This ID already signed up. Please check again.');
+	signUpForm.classList.add(HIDDEN_CLASSNAME);
+});
 
-
-function signUp(id, pw) {
-  userDb.push({ id: id, pw: pw });
-  writeDb();
+function successLogin(userId) {
+	console.log(userId);
+	signUpForm.classList.add(HIDDEN_CLASSNAME);
+	loginForm.classList.add(HIDDEN_CLASSNAME);
+	chat.classList.remove(HIDDEN_CLASSNAME);
+	loginIdInput.value = EMPTY_VALUE;
+	loginPwInput.value = EMPTY_VALUE;
+	socket.emit('connecting', userId);
 }
-
-function paintGreetings(userId) {
-  console.log(userId);
-  signUpForm.classList.add(HIDDEN_CLASSNAME);
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  chat.classList.remove(HIDDEN_CLASSNAME);
-}
-
-function saveUserId(userId) {
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERID_KEY, userId);
-}
-
-function authorizeUser(id, pw) {
-  let result = EMPTY_VALUE;
-  const _userDb = readDb();
-  for (let i = 0; i < userDb.length; i++) {
-    if (id === _userDb[i].id && pw === _userDb[i].pw) return result = id;
-
-  }
-}
+socket.on('connecting', (userId) => console.log(userId, ' successed to login'));
 
 function onLoginSubmit(event) {
-  event.preventDefault();
-  const id = loginIdInput.value;
-  const pw = loginPwInput.value;
-  if (authorizeUser(id, pw) === EMPTY_VALUE) {
-    return alert("Please check your id or pw.");
-  }
-  saveUserId(id);
-  paintGreetings(id);
-  loginIdInput.value = EMPTY_VALUE;
-  loginPwInput.value = EMPTY_VALUE;
+	event.preventDefault();
+	const id = loginIdInput.value;
+	const pw = loginPwInput.value;
+
+	loginEmit(id, pw);
 }
 
 function onSignUpSubmit(event) {
-  event.preventDefault();
-  const id = signUpIdInput.value;
-  const pw = signUpPwInput.value;
-  const check = signUpCheckInput.value;
-  if (pw !== check) {
-    return alert("Please enter pw & pw-check to be same");
-  }
-  signUp(id, pw);
-  signUpForm.classList.add(HIDDEN_CLASSNAME);
-  loginIdInput.value = id;
+	event.preventDefault();
+	const id = signUpIdInput.value;
+	const pw = signUpPwInput.value;
+	const check = signUpCheckInput.value;
+	if (pw !== check) {
+		return alert('Please enter pw & pw-check to be same');
+	}
+
+	signUpEmit(id, pw);
 }
 
 function showSignUp(event) {
-  event.preventDefault();
-  signUpForm.classList.remove(HIDDEN_CLASSNAME);
+	event.preventDefault();
+	signUpForm.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function hideSignUp(event) {
-  event.preventDefault();
-  signUpForm.classList.add(HIDDEN_CLASSNAME);
+	event.preventDefault();
+	signUpForm.classList.add(HIDDEN_CLASSNAME);
 }
 
-const savedUserId = localStorage.getItem(USERID_KEY);
-
-window.onload = function() {
-  if (savedUserId === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginSubmit.addEventListener("click", onLoginSubmit);
-    signUpButton.addEventListener("click", showSignUp);
-    signUpSubmit.addEventListener("click", onSignUpSubmit);
-
-  } else {
-    paintGreetings(savedUserId);
-  }
-
-}
+window.onload = function () {
+	loginForm.classList.remove(HIDDEN_CLASSNAME);
+	loginSubmit.addEventListener('click', onLoginSubmit);
+	signUpButton.addEventListener('click', showSignUp);
+	signUpSubmit.addEventListener('click', onSignUpSubmit);
+};
--- Node.js/socketChat/public/js/server.js
+++ Node.js/socketChat/public/js/server.js
@@ -1,38 +1,123 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const app = express();
-const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
+const mysql = require('mysql');
+
+const app = express();
 const PORT = 8080;
+const server = http.createServer(app);
+const io = new Server(server);
 const date = new Date();
-
-
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
-
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.broadcast.emit('hi');
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    _msg = `${msg} @[${hours}:${minutes}]`
-    io.emit('chat message', _msg);
-  });
-  socket.on('disconnect', () => {
-    console.log("user disconnected");
-  });
+const userDb = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'Pomcom1123@',
+	database: 'Users',
 });
+const AUTHDATA_TABLE = 'auth_data';
+let userId = 'unknown user';
 
-app.use(express.static(path.join(__dirname, "../../public")));
+app.set('views', '../../views'); //path 설정
+app.set('view engine', 'ejs'); //엔진 정의
+app.engine('html', require('ejs').renderFile); //엔진 사용
+
+let usersAuthData = [];
+
+function getUserDb(table) {
+	userDb.query(`SELECT * FROM ${table};`, function (error, auth_data) {
+		if (error) {
+			return console.log(error);
+		}
+		usersAuthData = auth_data;
+		console.log(usersAuthData);
+	});
+}
+function signUp(id, pw) {
+	userDb.query(
+		`INSERT INTO ${AUTHDATA_TABLE} (id, pw, authority) VALUES ('${id}', '${pw}', 'user');`,
+		function (error, auth_data) {
+			if (error) {
+				return console.log(error);
+			}
+		}
+	);
+	getUserDb(AUTHDATA_TABLE);
+}
+getUserDb(AUTHDATA_TABLE);
+
+app.use(express.static(path.join('../../public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "../../views/index.html"));
+	res.render('index.html');
 });
 
 server.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
+	console.log(`server is running on ${PORT}`);
+});
+
+io.emit('some event', {
+	someProperty: 'some value',
+	otherProperty: 'other value',
+});
+
+io.on('connection', (socket) => {
+	console.log(`${userId} connected`);
+
+	socket.on('login', (user) => {
+		let resId = '';
+		let auth = false;
+		let i = 0;
+		while (i < usersAuthData.length) {
+			console.log(user);
+			console.log({ id: usersAuthData[i].id, pw: usersAuthData[i].pw });
+
+			if (user.id === usersAuthData[i].id && user.pw === usersAuthData[i].pw) {
+				console.log('same');
+				resId = user.id;
+				auth = true;
+				break;
+			}
+			i = i + 1;
+		}
+		const res = { id: resId, auth: auth };
+		console.log(res);
+		io.emit('login', res);
+	});
+
+	socket.on('signUp', (req) => {
+		console.log('signing up');
+		var { id, pw } = req;
+		let unsigned = true;
+		let i = 0;
+		while (i < usersAuthData.length) {
+			if (id === usersAuthData[i].id) {
+				unsigned = false;
+				break;
+			}
+			i = i + 1;
+		}
+		if (unsigned) {
+			signUp(id, pw);
+		}
+		io.emit('signUp', unsigned);
+		console.log('emited');
+	});
+
+	socket.on('connecting', (id) => {
+		userId = id;
+		io.emit('connecting', userId);
+		socket.broadcast.emit(`${userId} logged in`);
+	});
+	socket.on('chat message', (msg) => {
+		console.log('message: ' + msg);
+		const hours = String(date.getHours()).padStart(2, '0');
+		const minutes = String(date.getMinutes()).padStart(2, '0');
+		const _msg = `#${userId}>> ${msg} @[${hours}:${minutes}]`;
+		io.emit('chat message', _msg);
+	});
+
+	socket.on('disconnect', () => {
+		console.log(`${userId} disconnected`);
+	});
 });
